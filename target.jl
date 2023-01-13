b404ada44879b1bfd8eeadb41479cccdcf9baa86
Oscar Cornish <oscar.cornish@outlook.com>
2023-01-13 14:02:21 +0000

    Added definitions of target (receiver) and microprotocols

---
+++ target.jl
@@ -0,0 +1,31 @@
+using Sockets
+using StaticArrays
+
+struct Target
+    ip::IPv4
+    covert_methods::Vector{String}
+    covert_options::Dict{String, Any}
+    AES_PSK::SVector{16, UInt8}
+    AES_IV::SVector{16, UInt8}
+    function Target(ip::IPv4, covert_methods::Vector{String}, covert_options::Dict{String, Any}, AES_PSK::SVector{16, UInt8}, AES_IV::SVector{16, UInt8})
+        return new(ip, covert_methods, covert_options, AES_PSK, AES_IV)
+    end
+end
+function Target(ip::AbstractString, covert_methods::Vector{String}, covert_options::Dict{String, Any}, AES_PSK::SVector{16, UInt8}, AES_IV::SVector{16, UInt8})
+    return Target(IPv4(ip), covert_methods, covert_options, AES_PSK, AES_IV)
+end
+
+target = Target(
+    "192.168.0.1",
+    ["TCP_ACK_Bounce", "IPv4_identification"],
+    Dict{String, Any}(
+        "TCP_ACK_Bounce" => Dict{String, Any}(
+            "listen_port" => 12873
+        ),
+        "IPv4_identification" => Dict{String, Any}(
+            "listen_port" => 12874
+        )
+    ),
+    SVector{16, UInt8}(0x0F, 0x0E, 0x0D, 0x0C, 0x0B, 0x0A, 0x09, 0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00),
+    SVector{16, UInt8}(0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F)
+)
