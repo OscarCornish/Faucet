dca6ab3d49b4cf4a841c4dd29ef837a0c8278ac0
Oscar Cornish <oscar.cornish@outlook.com>
2023-03-01 00:31:05 +0000

    Added target file parsing

--- src/target.jl
+++ src/target.jl
@@ -13,17 +13,45 @@ function Target(ip::AbstractString, covert_methods::Vector{String}, AES_PSK::SVe
     return Target(IPv4Addr(ip), covert_methods, AES_PSK, AES_IV)
 end
 
-target = Target(
-    "172.29.0.197",
-    ["IPv4_identification", "TCP_ACK_Bounce"],
-    SVector{16, UInt8}(0x0F, 0x0E, 0x0D, 0x0C, 0x0B, 0x0A, 0x09, 0x08, 0x07, 0x06, 0x05, 0x04, 0x03, 0x02, 0x01, 0x00),
-    SVector{16, UInt8}(0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F)
-)
-
 """
 Target file structure:
 ip: 'aaa.bbb.ccc.ddd'
 covert_methods: ['method1', 'method2', ...]
 AES_PSK: '000102030405060708090A0B0C0D0E0F'
+AES_IV: '000102030405060708090A0B0C0D0E0F'
+"""
+
+chunk(s::AbstractString, n::Int)::Vector{AbstractString} = [s[i:min(i + n - 1, end)] for i=1:n:length(s)]
+
+# Parse target from file structure to Target struct
+function parse_target_file(file::AbstractString)::Target
+    target = Dict{String, String}([attr => value for (attr, value) ∈ split.(split(readchomp(file), "\n"), ": ")])
+    if all(["ip", "covert_methods", "AES_PSK", "AES_IV"] .∉ Ref(keys(target)))
+        @error "Invalid arguments" keys(target)
+        throw(ArgumentError("Target file must contain ip, covert_methods, AES_PSK, and AES_IV"))
+    end
+
+    ip = split(target["ip"], "'")[2]
+    covert_methods = String.(split(split(split(target["covert_methods"], "['")[2], "']")[1], "', '"))
+
+    AES_PSK = SVector{16, UInt8}(parse.(UInt8, chunk(split(target["AES_PSK"], "'")[2], 2); base=16))
+    AES_IV = SVector{16, UInt8}(parse.(UInt8, chunk(split(target["AES_IV"], "'")[2], 2); base=16))
+    return Target(ip, covert_methods, AES_PSK, AES_IV)
+end
+
+if length(ARGS) > 0
+    target = parse_target_file(ARGS[1])
+else
+    @error "No target file provided"
+    throw(ArgumentError("No target file provided"))
+end
+
+@info "Target file parsed" target
+
+# Example target file
+"""
+ip: '10.20.30.9'
+covert_methods: ['IPv4_identification', 'TCP_ACK_Bounce']
+AES_PSK: '0F0E0D0C0B0A09080706050403020100'
 AES_IV: '000102030405060708090A0B0C0D0E0F'
 """
